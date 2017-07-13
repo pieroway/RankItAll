d25eda0274d93379608701389df48dd64fd501fe
Alan Pieroway <alan.pieroway@gmail.com>
2017-07-12 22:31:41 -0400

    preventing, as much as possible, two people becoming the moderator of as show and stepping on each others attempt

--- client/src/app/tvModerator/tvmoderator.component.ts
+++ client/src/app/tvModerator/tvmoderator.component.ts
@@ -18,6 +18,7 @@ export class TvModeratorComponent {
     public isLoading: boolean; 
     public userEmail: string;
     public isModerator: boolean = false; 
+    public hasModerator: boolean = false; 
     public votingServiceError: boolean = false;
     public errorMessage: string;
 
@@ -31,12 +32,8 @@ export class TvModeratorComponent {
         if(this.auth.userProfile && this.auth.isAuthenticated()) {
             this.userEmail = this.auth.isAuthenticated ? this.auth.userProfile.name : "";
             console.log(`userEmail = ${this.auth.userProfile.name}`);
-            if (this.userEmail) {
-                this.getModerator();
-            } else  {
-                this.isModerator = false;
-            }
         } 
+        this.getModerator();
     }
  
     addModerator(){
@@ -82,6 +79,42 @@ export class TvModeratorComponent {
     }
 
     getModerator() : void {
+        console.log(`getModerator() `);
+        let _that = this;
+        this.votingService.getModerator(this.showId).subscribe(
+            data => {
+                console.log(`data=${data}`);
+                if (data.length>0) {
+                    if (data.err) {
+                        _that.votingServiceError = true;
+                        _that.errorMessage = `${data.status} ${data.err.errno}: ${data.err.code}`;
+                    } else {
+                        _that.votingServiceError = false;
+                        console.log(data[0]);
+                        if(data[0]['email']) {
+                            _that.hasModerator = true;
+                            console.log(`hasModerator=${_that.hasModerator}`);
+                        }
+                        if(this.auth.isAuthenticated()) {
+                            _that.isModerator = data[0]['email'] === _that.userEmail ? true : false ;
+                            console.log(`isModerator=${_that.isModerator}`);
+                        }
+                    }
+                } else {
+                    _that.hasModerator = false;
+                    _that.isModerator = false;
+                }
+
+            },
+            err => {
+                _that.votingServiceError = true;
+                _that.errorMessage = 'Could not get moderator.';
+            }
+        );
+        
+    }
+
+    getModerator2() : void {
         if(this.auth.isAuthenticated()) {
             let _that = this;
             this.votingService.getModerator(this.showId).subscribe(
@@ -92,7 +125,7 @@ export class TvModeratorComponent {
                         _that.isModerator = false;
                     } else {
                         _that.votingServiceError = false;
-                        console.log(data);  
+                        _that.hasModerator = true ;
                         _that.isModerator = data[0]['email'] === _that.userEmail ? true : false ;
                     }
                 },
